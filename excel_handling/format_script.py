6b4cd1f0dbfd1545dc4cda2c3870befdbd4c4768
syedalihasany <49318734+syedalihasany@users.noreply.github.com>
2022-12-06 21:11:13 +0500

    logic added

--- excel_handling/format_script.py
+++ excel_handling/format_script.py
@@ -5,9 +5,51 @@
 
 import numpy
 import pandas as pd
-import os 
+import os
+import math
+
+def valid_row(df):
+    rows    = len(df.axes[0])
+    columns = len(df.axes[1])
+    count = 0
+    for row in range(rows):
+        count = 0
+        for column in range(columns):
+            if( type(df.iloc[row,column]) == str ):
+                count+=1
+                if (count >= 4):
+                    return row
+
+def valid_column(df):
+    rows    = len(df.axes[0])
+    columns = len(df.axes[1])
+    count = 0
+    for column in range(columns):
+        count = 0
+        for row in range(rows):
+            if( math.isnan(df.iloc[row,column]) ):
+                count+=1
+                if (count >= 5):
+                    return column
 
 
 df = pd.read_excel(r'unformatted.xlsx')
-df.to_excel('midformatted.xlsx')
-df = pd.read_excel(r'midformatted.xlsx')
+
+rows    = len(df.axes[0])
+columns = len(df.axes[1])
+
+start_row = valid_row(df)
+start_column = valid_column(df)
+
+print(start_row)
+print(start_column)
+
+for row in range(0,start_row):
+    df = df.drop(labels=[row], axis=0)
+
+df = df.drop(df.columns[start_column], axis=1)
+
+print(df)
+
+
+df.to_excel('newformatted.xlsx', header = False, index = False)
